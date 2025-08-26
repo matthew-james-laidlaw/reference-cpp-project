ce4bb7d79c5381a53523bcc62fe1556c5d9af1c7
Matthew Laidlaw <matthew.james.laidlaw@gmail.com>
2025-08-26 02:00:24 -0700

    force bad clang-tidy

--- apps/calculator/main.cpp
+++ apps/calculator/main.cpp
@@ -5,9 +5,12 @@
 
 auto main() -> int
 {
-    std::cout << std::format("1 + 2 = {}", arithmetic::add(1, 2)) << std::endl;
-    std::cout << std::format("3 - 2 = {}", arithmetic::sub(3, 2)) << std::endl;
-    std::cout << std::format("2 * 3 = {}", arithmetic::mul(2, 3)) << std::endl;
-    std::cout << std::format("6 / 3 = {}", arithmetic::div(6, 3)) << std::endl;
-    return 0;
+    double d = 3.14159;
+    int x = d;
+    return x;
+    // std::cout << std::format("1 + 2 = {}", arithmetic::add(1, 2)) << std::endl;
+    // std::cout << std::format("3 - 2 = {}", arithmetic::sub(3, 2)) << std::endl;
+    // std::cout << std::format("2 * 3 = {}", arithmetic::mul(2, 3)) << std::endl;
+    // std::cout << std::format("6 / 3 = {}", arithmetic::div(6, 3)) << std::endl;
+    // return 0;
 }
